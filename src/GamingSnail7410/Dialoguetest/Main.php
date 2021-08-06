75284dcf63790a87591a054e1c037317b09ef360
TheGamingSnail <87595446+TheGamingSnail@users.noreply.github.com>
2021-08-06 05:38:45 +0300

    maybe this works?

--- src/GamingSnail7410/Dialoguetest/Main.php
+++ src/GamingSnail7410/Dialoguetest/Main.php
@@ -14,33 +14,23 @@ use pocketmine\command\Command;
 
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase implements Listener{
-   public function onEnabled(){
-   	$this->getServer()->getPluginManager()->registerEvents($this,$this);
-   	$this->getLogger()->info("Plugin is Enabled");
-
-   }
-   
-   public function onDisabled(){
-   	$this->getLogger()->info("Plugin is Disabled");
-   }
    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{switch($cmd->getName()){
-    case "test1":
+    case "test1";
       $sender->sendMessage("Test Dialogue 1");
     break;
    }
 
-   return true:
+   return true;
 
 }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{switch($cmd->getName()){
-    case "test2":
+    case "test2";
       $sender->sendMessage("Test Dialogue 2");
     break;
           
    }
 
-   return true:
+   return true;
 
 }
