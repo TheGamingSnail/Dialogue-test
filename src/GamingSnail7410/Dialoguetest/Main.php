32df51ffca76aabec25a4f8a35f314da03c05675
TheGamingSnail <87595446+TheGamingSnail@users.noreply.github.com>
2021-08-06 06:06:46 +0300

    ugh

--- src/GamingSnail7410/Dialoguetest/Main.php
+++ src/GamingSnail7410/Dialoguetest/Main.php
@@ -8,23 +8,38 @@ use pocketmine\Player;
 
 use pocketmine\Server;
 
+use pocketmine\event\Listener;
+
 use pocketmine\command\Command;
 
 use pocketmine\command\CommandSender;
 
+class Main extends PluginBase implements Listener{
+   public function onEnabled(){
+   	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+   	$this->getLogger()->info("Dialogue test is on");
+
+   }
+   
+   public function onDisabled(){
+   	$this->getLogger()->info("Dialogue test turned off");
+   }
    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
    	switch($cmd->getName()){
-   		case "test1":
-   		  $sender->sendMessage("This is dialogue test 1!");
+   		case "tes1t":
+   		  $sender->sendMessage("This is dialogue test 1");
    		break;
    	}
     return true;
    }
+   
+      }
    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
    	switch($cmd->getName()){
-   		case "test2":
-   		  $sender->sendMessage("This is dialogue test 2!");
+   		case "tes1t":
+   		  $sender->sendMessage("This is dialogue test 1");
    		break;
    	}
     return true;
    }
+}
